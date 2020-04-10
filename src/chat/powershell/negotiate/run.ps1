9b49d7c2b1f496e7e20e75fbbf14bd8dc43fd9ec
Brandon H <brandon@bc3tech.net>
2020-04-10 08:46:51 -0700

    readying for azure deploy

--- src/chat/powershell/negotiate/run.ps1
+++
@@ -1,8 +0,0 @@
-using namespace System.Net
-
-param($Request, $TriggerMetadata, $ConnectionInfo)
-
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $ConnectionInfo
-})
